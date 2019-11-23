542d9b5316ee37d0062ac75711562dd92ba558f7
Olzhas <zait.olzhas@gmail.com>
2019-11-23 13:38:30 +0600

    End of section 5

---
+++ client-app/src/features/activities/details/ActivityDetail.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, Image, Button, ButtonGroup } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface IProps {    
+    activity:IActivity;
+    setEditMode:(editMode:boolean)=>void;
+    setSelectedActivity:(activity:IActivity|null)=>void;
+}
+
+export const ActivityDetail:React.FC<IProps> = ({activity,setEditMode,setSelectedActivity}) => {
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <ButtonGroup widths={2}>
+          <Button onClick={()=>setEditMode(true)} basic color="blue" content="Edit" />
+          <Button onClick={()=>setSelectedActivity(null)} basic color="grey" content="Cancel" />
+        </ButtonGroup>
+      </Card.Content>
+    </Card>
+  );
+};
